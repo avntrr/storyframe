af40fba4d340f10d20825e432b09ff49ae8459d7
Ilham Iqbal <54851225+avntrr@users.noreply.github.com>
2026-04-02 10:01:31 +0700

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StoryFrame from '../storyframe.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StoryFrame />
+  </React.StrictMode>
+)
